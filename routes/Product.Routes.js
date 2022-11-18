dcb4a7d248a2c56395c05fdf172191f682003955
nkrpradhan@gmail.com <nkrpradhan@gmail.com>
2022-11-18 17:51:23 +0000

    model name change

--- routes/Product.Routes.js
+++ routes/Product.Routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const asyncHandler = require("express-async-handler");
-const Product = require("../models/Products.Model");
+const Product = require("../models/products.model");
 
 const productRouter = express.Router();
 
