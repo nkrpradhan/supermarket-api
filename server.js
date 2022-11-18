07924a4393481e1f901e6ed8ddb5f649e79a33b7
nkrpradhan@gmail.com <nkrpradhan@gmail.com>
2022-11-18 15:47:25 +0000

    test db

--- server.js
+++ server.js
@@ -1,28 +1,14 @@
-import express from "express";
-import dotenv from "dotenv";
-import connectDatabase from "./config/MongoDB.js";
-import ImportData from "./DataImport.js";
-import productRouter from "./routes/Product.Routes.js";
-import { errorHandler, notFound } from "./middleware/Error.js";
-import userRouter from "./routes/User.Routes.js";
-import shoppingListRouter from "./routes/ShoppingList.Routes.js";
-import cors from "cors";
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDatabase = require("./config/MongoDB.js");
+
+const cors = require("cors");
 
 dotenv.config();
 connectDatabase();
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-//API
-app.use("/api/import", ImportData);
-app.use("/api/products", productRouter);
-app.use("/api/users", userRouter);
-app.use("/api/shopping-list/", shoppingListRouter);
-
-//ERROR HANDLERS
-app.use(notFound);
-app.use(errorHandler);
 
 app.get("/", (req, res) => {
   res.send("API is live...");
