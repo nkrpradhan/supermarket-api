b06b25774863802e40c31f139f5dc0c9c6d5960c
nkrpradhan@gmail.com <nkrpradhan@gmail.com>
2022-11-19 10:09:14 +0000

    db config

--- config/mongoDB.js
+++ config/mongoDB.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const connectDatabase = async () => {
   try {
-    const connection = mongoose.connect(process.env.MONGO_URL);
+    const connection = mongoose.connect(process.env.MONGO_URL, {
+      dbName: process.env.DB_NAME,
+    });
     console.log("Mongo Connected");
   } catch (err) {
     console.log(`Error: ${err.message}`);
